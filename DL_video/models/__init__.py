c9687c9d1cc9d44bfbb3063750f1f69be0afd357
manhmitcf <manhmit1221@gmail.com>
2026-07-21 21:44:22 +0700

    Flatten models directory: move model files out of model_zoo directly into DL_video/models/

--- DL_video/models/__init__.py
+++ DL_video/models/__init__.py
@@ -1,10 +1,10 @@
 from .base_backbone import BaseBackbone
 from .video_model import VideoModel
-from .model_zoo.S3D import S3D
-from .model_zoo.vit3D import ViT3D
-from .model_zoo.vivit import ViViT
-from .model_zoo.ResNet3D import generate_model as generate_resnet3d
-from .model_zoo.MobileVitV1 import mobilevit_xxs, mobilevit_xs, mobilevit_s
+from .S3D import S3D
+from .vit3D import ViT3D
+from .vivit import ViViT
+from .ResNet3D import generate_model as generate_resnet3d
+from .MobileVitV1 import mobilevit_xxs, mobilevit_xs, mobilevit_s
 
 __all__ = [
     "BaseBackbone",
