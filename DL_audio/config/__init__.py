4b0da24db3e7d103615e70355d79b4fe400271c8
Hoang Phi Hung <hoangphihung0404@gmail.com>
2026-07-24 00:18:52 +0700

    Add post-training Hugging Face artifact upload

--- DL_audio/config/__init__.py
+++ DL_audio/config/__init__.py
@@ -1,7 +1,9 @@
 from .train_config import TrainConfig, AudioFeaturesConfig, SplitterConfig
+from .artifact_upload_config import ArtifactUploadConfig
 
 __all__ = [
     "TrainConfig",
     "AudioFeaturesConfig",
     "SplitterConfig",
+    "ArtifactUploadConfig",
 ]
