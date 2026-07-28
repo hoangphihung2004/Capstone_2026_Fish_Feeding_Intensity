45a9a11d495f3dd8b3d47051f067259335102759
Hoang Phi Hung <hoangphihung0404@gmail.com>
2026-07-29 01:40:50 +0700

    Add audio EfficientNetB0 backbone

--- DL_audio/models/__init__.py
+++ DL_audio/models/__init__.py
@@ -3,6 +3,7 @@ from .cnn14_mobilev2 import Cnn14MobileV2
 from .cnn14_mobilev2_1p9m import Cnn14MobileV2_1P9M
 from .mobilenet_v2 import MobileNetV2
 from .resnet22 import ResNet22
+from .efficientnet_b0 import EfficientNetB0
 from .audio_model import AudioModel
 from .panns_cnn10 import PANNS_Cnn10
 from .panns_cnn6 import PANNS_Cnn6
@@ -14,6 +15,7 @@ __all__ = [
     "Cnn14MobileV2_1P9M",
     "MobileNetV2",
     "ResNet22",
+    "EfficientNetB0",
     "AudioModel",
     "PANNS_Cnn10",
     "PANNS_Cnn6",
