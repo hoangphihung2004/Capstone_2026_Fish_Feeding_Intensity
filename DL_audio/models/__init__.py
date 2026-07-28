076f63b8d186d24c0d8727040e82acfe0e9e1e9b
Hoang Phi Hung <hoangphihung0404@gmail.com>
2026-07-29 00:24:14 +0700

    Add audio MobileNetV2 backbone

--- DL_audio/models/__init__.py
+++ DL_audio/models/__init__.py
@@ -1,6 +1,7 @@
 from .base_backbone import BaseBackbone
 from .cnn14_mobilev2 import Cnn14MobileV2
 from .cnn14_mobilev2_1p9m import Cnn14MobileV2_1P9M
+from .mobilenet_v2 import MobileNetV2
 from .audio_model import AudioModel
 from .panns_cnn10 import PANNS_Cnn10
 from .panns_cnn6 import PANNS_Cnn6
@@ -10,6 +11,7 @@ __all__ = [
     "BaseBackbone",
     "Cnn14MobileV2",
     "Cnn14MobileV2_1P9M",
+    "MobileNetV2",
     "AudioModel",
     "PANNS_Cnn10",
     "PANNS_Cnn6",
