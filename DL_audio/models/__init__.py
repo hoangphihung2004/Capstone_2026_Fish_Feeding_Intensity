163f074b64ec3719a898a8a0890117533ac40929
Hoang Phi Hung <hoangphihung0404@gmail.com>
2026-07-29 00:55:53 +0700

    Add audio ResNet22 backbone

--- DL_audio/models/__init__.py
+++ DL_audio/models/__init__.py
@@ -2,6 +2,7 @@ from .base_backbone import BaseBackbone
 from .cnn14_mobilev2 import Cnn14MobileV2
 from .cnn14_mobilev2_1p9m import Cnn14MobileV2_1P9M
 from .mobilenet_v2 import MobileNetV2
+from .resnet22 import ResNet22
 from .audio_model import AudioModel
 from .panns_cnn10 import PANNS_Cnn10
 from .panns_cnn6 import PANNS_Cnn6
@@ -12,6 +13,7 @@ __all__ = [
     "Cnn14MobileV2",
     "Cnn14MobileV2_1P9M",
     "MobileNetV2",
+    "ResNet22",
     "AudioModel",
     "PANNS_Cnn10",
     "PANNS_Cnn6",
